090092849a79516e6c6c766fbf3c27bc4f74938b
lsliwko <lsliwko@gmail.com>
2014-10-03 12:44:19 +0100

    clone

---
+++ src/com/axisapps/subwaymapsaustralia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.axisapps.subwaymapsaustralia;
+
+import org.apache.cordova.*;
+import android.os.Bundle;
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
